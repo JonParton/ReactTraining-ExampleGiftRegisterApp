78f6b2d034b01511b16408bcf7aad5b5b0f6373c
Jonathan Parton <jon.parton@outlook.com>
2020-12-03 23:12:29 +0000

    Added back link to Github Code from Example

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
+  Box,
   Hidden,
   IconButton,
   Toolbar,
+  Tooltip,
   Typography,
+  Button,
+  Link as MUILink,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import { useSetRecoilState } from "recoil";
 import { mobileMenuOpenState } from "./SideNav/SideNavAtom";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -51,6 +57,28 @@ export const NavBar: React.FunctionComponent = () => {
           <Typography variant="h6" className={classes.title}>
             Example Gift Register
           </Typography>
+          <Tooltip
+            title="See the code on GitHub"
+            aria-label="See the code on GitHub"
+            classes={{ tooltip: classes.tooltip }}
+          >
+            <MUILink
+              href="https://github.com/JonParton/ReactTraining-ExampleGiftRegisterApp"
+              color="inherit"
+            >
+              <Button
+                component="span"
+                startIcon={<GitHubIcon />}
+                color="inherit"
+              >
+                <Box display={{ xs: "none", sm: "block" }}>
+                  <Typography variant="h6" className={classes.title}>
+                    See the code on GitHub
+                  </Typography>
+                </Box>
+              </Button>
+            </MUILink>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </div>
